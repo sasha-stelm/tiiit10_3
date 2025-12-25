c7f98ae2c5d9aaf685b71366c37d11ea355ad168
sasha-stelm <sasha.stelmashuk.07@mail.ru>
2025-12-25 11:01:27 +0300

    Update emails.cpp

--- emails.cpp
+++ emails.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <iterator>
 
 using namespace std;
 
@@ -10,13 +11,15 @@ int main() {
 
     regex email_pattern(R"(\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b)");
 
-    sregex_iterator it(text.begin() text.end(), email_pattern);
+    sregex_iterator it(text.begin(), text.end(), email_pattern);
     sregex_iterator end;
 
     cout << "Found emails:" << endl;
     while (it != end) {
         smatch match = *it;
         cout << match.str() << endl;
-        +it;
+        ++it; 
     }
+    
+    return 0;
 }
